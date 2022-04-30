d5f03ca5604efc942a2cd556d5bb54ac1daaa5e0
Gabriel <gabrielmarcano120@gmail.com>
2022-04-30 15:27:20 -0400

    Added helper functions (incomplete)

---
+++ src/client/js/helperFunctions.js
@@ -0,0 +1,12 @@
+// Return float with 3 decimal places
+const threeDecimals = (num) => {
+    return Number.parseFloat(num).toFixed(3);
+}
+
+// Check if the date is within this week or if its in the future
+const thisWeek = (today, departure) => {
+
+}
+
+export { threeDecimals, thisWeek }
+

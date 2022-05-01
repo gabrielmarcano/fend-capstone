38c8452cace13b9795d50c12ba7c01ef6910356f
Gabriel <gabrielmarcano120@gmail.com>
2022-04-30 23:32:56 -0400

    New test files

--- __test__/testCheckDate.spec.js
+++ __test__/testCheckDate.spec.js
@@ -1,16 +1,16 @@
 // Import the js file to test
-import { checkUrlInput } from "../src/client/js/urlChecker"
+import { thisWeek } from "../src/client/js/checkDate"
 
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
-describe("Testing the text format", () => {
+describe("Testing the date checker", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.  
-    test("Testing the formatText() function", () => {
+    test("Testing thisWeek() function", () => {
            // Define the input for the function, if any, in the form of variables/array
-           const input = 'www.theverge.com/2022/4/28/23043011/snapchat-pixy-drone-hands-on'
+           const input = new Date();
            // Define the expected output, if any, in the form of variables/array
-           const output = true
+           const output = true;
            // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
            // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-           expect(checkUrlInput(input)).toEqual(output);
+           expect(thisWeek(input)).toEqual(output);
 })});

95b7152548918648bd99e43b50019606be810399
Gabriel <gabrielmarcano120@gmail.com>
2022-04-30 21:46:00 -0400

    Added placename as URI component

--- src/client/js/getCoords.js
+++ src/client/js/getCoords.js
@@ -1,7 +1,7 @@
 // GeoNames API action function
 async function callGeoNames(placename, key) {
     const response = await Client.getData(
-        `http://api.geonames.org/searchJSON?username=${key}&maxRows=10&q=${placename}`
+        `http://api.geonames.org/searchJSON?username=${key}&maxRows=10&q=${encodeURIComponent(placename)}`
     );
     
     try {

be25c2e44ad5e39a04f9474b89e5f64cd62ad268
Gabriel <gabrielmarcano120@gmail.com>
2022-04-30 21:08:47 -0400

    Imported functions, added event listener

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,12 @@
-import { checkUrlInput } from './js/urlChecker'
-import { handleSubmit } from './js/formHandler'
+import { thisWeek } from './js/checkDate'
+import { getData } from './js/getData'
+import { postData } from './js/postData'
+import { callGeoNames } from './js/geoNames'
+import { callCurrentWeatherBit } from './js/currentWeather'
+import { callForecastWeatherBit } from './js/forecastWeather'
+import { callPixabay } from './js/getImages'
+import { updateUI } from './js/updateUI'
+import { main } from './js/app'
 
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -7,7 +14,17 @@ import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 
+// Add event listener to button
+document.getElementById("search").addEventListener("click", ()=>{console.log('click')})
+
 export {
-    checkUrlInput,
-    handleSubmit
+    thisWeek,
+    getData,
+    postData,
+    callGeoNames,
+    callCurrentWeatherBit,
+    callForecastWeatherBit,
+    callPixabay,
+    updateUI,
+    main,
 }

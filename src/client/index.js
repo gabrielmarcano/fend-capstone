d74b5907a911688724eabcacc118e3df0a72437c
Gabriel <gabrielmarcano120@gmail.com>
2022-04-30 21:46:15 -0400

    Removed old weather functions

--- src/client/index.js
+++ src/client/index.js
@@ -2,8 +2,6 @@ import { thisWeek } from './js/checkDate'
 import { getData } from './js/getData'
 import { postData } from './js/postData'
 import { callGeoNames } from './js/getCoords'
-import { callCurrentWeatherBit } from './js/currentWeather'
-import { callForecastWeatherBit } from './js/forecastWeather'
 import { callWeatherBit } from './js/getWeather'
 import { callPixabay } from './js/getImages'
 import { updateUI } from './js/updateUI'
@@ -16,15 +14,13 @@ import './styles/footer.scss'
 import './styles/header.scss'
 
 // Add event listener to button
-document.getElementById("search").addEventListener("click", ()=>{console.log('click')})
+document.getElementById("search").addEventListener("click", main);
 
 export {
     thisWeek,
     getData,
     postData,
     callGeoNames,
-    callCurrentWeatherBit,
-    callForecastWeatherBit,
     callWeatherBit,
     callPixabay,
     updateUI,

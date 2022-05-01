9a27cd30f9b321f6613e30216a9d96f1b057e518
Gabriel <gabrielmarcano120@gmail.com>
2022-04-30 23:43:04 -0400

    localStorage only update function

--- src/client/js/update.js
+++ src/client/js/update.js
@@ -1,42 +1,34 @@
 // Update UI function
-async function updateUI() {
-    const request = await fetch('/data');
-    try {
-        const serverData = await request.json();
+function updateUI() {
+    const localData = JSON.parse(localStorage.getItem('trip'));
 
-        const localData = JSON.parse(localStorage.getItem('trip'));
+    let results = document.getElementById('results');
+    let fragment = document.createDocumentFragment();
 
-        let results = document.getElementById('results');
-        let fragment = document.createDocumentFragment();
+    let destination = document.createElement('div');
+    destination.innerText = localData.destination;
 
-        let destination = document.createElement('div');
-        destination.innerText = localData.destination;
+    let departure = document.createElement('div');
+    departure.innerText = localData.departure;
 
-        let departure = document.createElement('div');
-        departure.innerText = localData.departure;
+    let weather = document.createElement('div');
+    weather.innerText = localData.weather;
 
-        let weather = document.createElement('div');
-        weather.innerText = localData.weather;
+    let temperature = document.createElement('div');
+    temperature.innerText = localData.temperature;
 
-        let temperature = document.createElement('div');
-        temperature.innerText = localData.temperature;
+    let image = document.createElement('img');
+    image.setAttribute('src', localData.image);
+    image.setAttribute('alt', localData.destination);
+    image.setAttribute('width', '300');
+    
+    fragment.appendChild(destination);
+    fragment.appendChild(departure);
+    fragment.appendChild(weather);
+    fragment.appendChild(temperature);
+    fragment.appendChild(image);
 
-        let image = document.createElement('img');
-        image.setAttribute('src', localData.image);
-        image.setAttribute('alt', localData.destination);
-        image.setAttribute('width', '300');
-        
-        fragment.appendChild(destination);
-        fragment.appendChild(departure);
-        fragment.appendChild(weather);
-        fragment.appendChild(temperature);
-        fragment.appendChild(image);
-
-        results.appendChild(fragment);
-
-    } catch (error) {
-        console.log("Error: ", error);
-    }
+    results.appendChild(fragment);
 }
 
 export { updateUI }

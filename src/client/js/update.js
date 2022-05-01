52f87daf01c0b5f5aeea5036b74c4c594b9fc76e
Gabriel <gabrielmarcano120@gmail.com>
2022-04-30 23:34:27 -0400

    New update file

---
+++ src/client/js/update.js
@@ -0,0 +1,42 @@
+// Update UI function
+async function updateUI() {
+    const request = await fetch('/data');
+    try {
+        const serverData = await request.json();
+
+        const localData = JSON.parse(localStorage.getItem('trip'));
+
+        let results = document.getElementById('results');
+        let fragment = document.createDocumentFragment();
+
+        let destination = document.createElement('div');
+        destination.innerText = localData.destination;
+
+        let departure = document.createElement('div');
+        departure.innerText = localData.departure;
+
+        let weather = document.createElement('div');
+        weather.innerText = localData.weather;
+
+        let temperature = document.createElement('div');
+        temperature.innerText = localData.temperature;
+
+        let image = document.createElement('img');
+        image.setAttribute('src', localData.image);
+        image.setAttribute('alt', localData.destination);
+        image.setAttribute('width', '300');
+        
+        fragment.appendChild(destination);
+        fragment.appendChild(departure);
+        fragment.appendChild(weather);
+        fragment.appendChild(temperature);
+        fragment.appendChild(image);
+
+        results.appendChild(fragment);
+
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+export { updateUI }

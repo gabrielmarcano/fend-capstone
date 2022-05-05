a46c585e40c92a0060febdec01cdf0d628e07a5e
Gabriel <gabrielmarcano120@gmail.com>
2022-05-05 14:11:08 -0400

    Current update logic

--- src/client/js/update.js
+++ src/client/js/update.js
@@ -1,34 +1,58 @@
 // Update UI function
-function updateUI() {
-    const localData = JSON.parse(localStorage.getItem('trip'));
+async function updateUI() {
 
-    let results = document.getElementById('results');
-    let fragment = document.createDocumentFragment();
+    // Get the data from the server
+    let data = await Client.getData('/data');
 
-    let destination = document.createElement('div');
-    destination.innerText = localData.destination;
+    // Get data from localStorage
+    // const localData = JSON.parse(localStorage.getItem('trip'));
 
-    let departure = document.createElement('div');
-    departure.innerText = localData.departure;
+    try {
 
-    let weather = document.createElement('div');
-    weather.innerText = localData.weather;
+        // Get the results wrapper element
+        let results = document.getElementById('results');
 
-    let temperature = document.createElement('div');
-    temperature.innerText = localData.temperature;
+        // Create a document fragment
+        let fragment = document.createDocumentFragment();
 
-    let image = document.createElement('img');
-    image.setAttribute('src', localData.image);
-    image.setAttribute('alt', localData.destination);
-    image.setAttribute('width', '300');
+        // Create div elements
+        let destination, departure, weather, temperature;
+        destination = departure = weather = temperature = document.createElement('div');
+
+        // Fill the destination data element
+        destination.innerText = data.destination;
+
+        // Fill the departure data element
+        departure.innerText = data.departure;
+
+        // Fill the weather data element
+        weather.innerText = data.weather;
+
+        // Fill the temperature data element
+        temperature.innerText = data.temperature;
+
+        // Fill the image data element
+        let image = document.createElement('img');
+        image.setAttribute('src', data.image);
+        image.setAttribute('alt', data.destination);
+        image.setAttribute('width', '300');
+        
+        // Append to the document fragment
+        fragment.appendChild(destination);
+        fragment.appendChild(departure);
+        fragment.appendChild(weather);
+        fragment.appendChild(temperature);
+        fragment.appendChild(image);
+
+        // Append the document fragment to the UI
+        results.appendChild(fragment);
+
+    } catch(error) {
+        console.log("Error: ", error);
+    }
+
+
     
-    fragment.appendChild(destination);
-    fragment.appendChild(departure);
-    fragment.appendChild(weather);
-    fragment.appendChild(temperature);
-    fragment.appendChild(image);
-
-    results.appendChild(fragment);
 }
 
 export { updateUI }

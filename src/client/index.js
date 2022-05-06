0cc2d2d249c88049d584cc2ac11c9b8c91546e1c
Gabriel <gabrielmarcano120@gmail.com>
2022-05-06 16:54:38 -0400

    Added function to update date input

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import { thisWeek } from './js/checkDate'
+import { updateDateInput } from './js/inputToday'
 import { getData } from './js/getData'
 import { postData } from './js/postData'
 import { callGeoNames } from './js/getCoords'
@@ -13,11 +14,15 @@ import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 
+// Update default input date
+document.addEventListener("DOMContentLoaded", updateDateInput);
+
 // Add event listener to button
 document.getElementById("search").addEventListener("click", main);
 
 export {
     thisWeek,
+    updateDateInput,
     getData,
     postData,
     callGeoNames,
---
+++ src/client/js/inputToday.js
@@ -0,0 +1,35 @@
+function updateDateInput() {
+    // Helper function to add the 0 at the left
+    function pad(n) {
+        return n<10 ? '0'+n : n;
+    }
+
+    // Date input element
+    let dateInput = getElementById('departure');
+
+    // This moment date
+    let now = new Date();
+
+    // This year
+    let year = now.getFullYear();
+
+    // Next year
+    let nextYear = now.getFullYear()+1;
+
+    // This month
+    let month = now.getMonth()+1;
+
+    // Today
+    let day = now.getDate();
+
+    // Value today
+    dateInput.setAttribute('value', `${pad(year)}-${pad(month)}-${pad(day)}`)
+
+    // Min value today
+    dateInput.setAttribute('min', `${pad(year)}-${pad(month)}-${pad(day)}`)
+
+    // Max value to next year
+    dateInput.setAttribute('max', `${pad(nextYear)}-${pad(month)}-${pad(day)}`)
+}
+
+export { updateDateInput }

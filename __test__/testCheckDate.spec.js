7fab76e46d4c0fd0fc6c58410beb099f4800733a
Gabriel <gabrielmarcano120@gmail.com>
2022-05-06 19:30:59 -0400

    Added fetch mock to tests

--- __test__/testCheckDate.spec.js
+++ __test__/testCheckDate.spec.js
@@ -1,17 +1,11 @@
 // Import the js file to test
 import { thisWeek } from "../src/client/js/checkDate"
 
-// The describe() function takes two arguments - a string description, and a test suite as a callback function.  
-// A test suite may contain one or more related tests    
-describe("Testing the date checker", () => {
-    // The test() function has two arguments - a string description, and an actual test as a callback function.  
-    test("Testing thisWeek() function", () => {
-           // Define the input for the function, if any, in the form of variables/array
-           const input = new Date();
-           input.setFullYear(2050);
-           // Define the expected output, if any, in the form of variables/array
-           const output = false;
-           // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-           // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-           expect(thisWeek(input)).toEqual(output);
-})});
+describe("Testing the date checker function", () => {
+    test("The function returns true", () => {
+
+        const input = new Date(2050, 5);
+
+        expect(thisWeek(input)).toBeFalsy();
+    })
+});
--- __test__/testGetData.spec.js
+++ __test__/testGetData.spec.js
@@ -1,14 +1,12 @@
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
+
 // Import the js file to test
 import { getData } from "../src/client/js/getData"
 
-// The describe() function takes two arguments - a string description, and a test suite as a callback function.  
-// A test suite may contain one or more related tests    
-describe("Testing the get functionality", () => {
-    // The test() function has two arguments - a string description, and an actual test as a callback function.  
-    test("Testing the GET server function", () => {
-           // Define the input for the function, if any, in the form of variables/array
-           const input = '/data'
-           // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-           // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-           expect(getData(input)).toBeDefined();
-})});
+describe("Testing the getData functionality", () => {  
+    test("The data from the server is defined", () => {
+
+        expect(getData('http://localhost:8081/data')).toBeDefined();
+    })
+});

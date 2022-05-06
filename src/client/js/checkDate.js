58c0f85ea7ff136d229111b3906e8150a77c1368
Gabriel <gabrielmarcano120@gmail.com>
2022-05-06 16:21:42 -0400

    Finished checkDate function

--- src/client/js/checkDate.js
+++ src/client/js/checkDate.js
@@ -1,23 +1,22 @@
 // Check if the date is within this week or if its in the future
 const thisWeek = (departure) => {
 
-    // Create a new date instance dynamically with JS
-    let d = new Date();
-    let today = d.getMonth()+1 + "/" + d.getDate() + "/" + d.getFullYear();
+    // Create a new date instance for today
+    let today = new Date();
     
-    const departureTimeStamp = (new Date(departure)).getTime();
+    // Get the time stamps
+    const departureTimeStamp = departure.getTime();
     const todayTimeStamp = today.getTime();
 
-    const microSecondsDiff = Math.abs(departureTimeStamp - todayTimeStamp);
+    // Difference in microseconds
+    const microDifference = Math.abs(departureTimeStamp - todayTimeStamp);
 
-    // Math.round is used instead of Math.floor to account for certain DST cases
-    // Number of milliseconds per day =
-    //   24 hrs/day * 60 minutes/hour * 60 seconds/minute * 1000 ms/second
-    const daysDiff = Math.round(microSecondsDiff / (1000 * 60 * 60  * 24));
+    // Number of milliseconds per day = 24 hrs/day * 60 minutes/hour * 60 seconds/minute * 1000 ms/second
+    // Difference in days
+    const daysDifference = (Math.round(microDifference / (1000 * 60 * 60  * 24)))+1;
 
-    console.log(daysDiff+1);
-
-    if (daysDiff > 7) {
+    // If greater than 7 days of difference, its in the future, else is this week
+    if (daysDifference > 7) {
         return false;
     } else {
         return true;
